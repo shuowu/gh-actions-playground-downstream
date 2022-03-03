24764a4be9314bc98fcafdbc1767cf83b20ea3e0
Shuo Wu <wushuo2010@gmail.com>
2022-03-03 00:20:22 -0500

    aaa

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 const fs = require('fs');
 fs.mkdirSync('./pr');
 fs.writeFileSync('./pr/context', JSON.stringify({ a: 1 }));
+
+// github.rest.repos.merge({
+//   owner: context.repo.owner,
+//   repo: context.repo.repo,
+//   base: 'main',
+//   head: 'update-samples-generated'
+// });
